b55f993fbc7ffbbede252a590bf88aacea736062
nikolai-dimitrov <pro9@abv.bg>
2023-05-23 14:07:48 +0300

    modified:   cars-manager/cars/settings.py

--- cars-manager/cars/settings.py
+++ cars-manager/cars/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-CSRF_TRUSTED_ORIGINS = ['http://*.localhost:81']
+CSRF_TRUSTED_ORIGINS = ['http://cars-manager.tk', 'https://cars-manager.tk']
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 # ALLOWED_HOSTS = []
 # CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split()
